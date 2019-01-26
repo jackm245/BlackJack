be95cd739d06b2077de6ed524096865a1f780c62
Jack <jack.morgan2@challoners.org>
2019-01-26 14:30:53 +0000

    Create cards.py

---
+++ cards.py
@@ -0,0 +1,8 @@
+#A list of card suits
+SuitList=["Hearts", "Diamonds", "Clubs", "Spades"]
+
+#A list of the card face values
+ListOfValaues=["Ace", "2", "3","4","5", "6","7", "8", "9", "10", "Jack", "Queen", "King"]
+
+#an empty list this gets appended with the cardsa that have been picked
+AlreadyPickedCards=[]
